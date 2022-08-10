1ecb18e312e6405e363e25e1ee42de5e96e3332c
Anwar Sarmiento <asarmiento@sistemasamigableslatam.com>
2022-08-10 15:18:18 -0600

    fix: cambio en linea de proceso

--- app/Console/Commands/SendFilesFIFCO/Faca/SaleFormat.php
+++ app/Console/Commands/SendFilesFIFCO/Faca/SaleFormat.php
@@ -45,7 +45,7 @@ class SaleFormat extends Command
 		set_time_limit(0);
 		ini_set('memory_limit','94G');
 		$fh=fopen(storage_path("app".DIRECTORY_SEPARATOR."FIFCO".DIRECTORY_SEPARATOR."salesFormat.txt"),'w') or die("Se produjo un error al crear el archivo");
-		$invoices=Invoice::where('invoice_type_id',2)->where('ind_estado','aceptado')->where('date','2022-08-09')->get();
+		$invoices=Invoice::where('invoice_type_id',2)->where('ind_estado','aceptado')->where('date',Carbon::now()->toDateString())->get();
 		Log::info("ventas ".json_encode($invoices));
 		$sysconf=Sysconf::first();
 		foreach ($invoices AS $invoice) {
@@ -105,8 +105,8 @@ class SaleFormat extends Command
 
 		$local = Storage::disk('local')->path("FIFCO".DIRECTORY_SEPARATOR."salesFormat.txt");
 
-//	Storage::disk('sftp')->put(DIRECTORY_SEPARATOR."ventas".Carbon::now()->format('dmY').".txt",fopen($local,'r+'));
-	Storage::disk('sftp')->put(DIRECTORY_SEPARATOR."ventas09082022.txt",fopen($local,'r+'));
+	Storage::disk('sftp')->put(DIRECTORY_SEPARATOR."ventas".Carbon::now()->format('dmY').".txt",fopen($local,'r+'));
+	//Storage::disk('sftp')->put(DIRECTORY_SEPARATOR."ventas02082022.txt",fopen($local,'r+'));
 
 	}
 }
